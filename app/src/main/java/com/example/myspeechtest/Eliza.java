7faa7a996d25422a78f6e66154d99d811bb1a1f6
Axel Klinger <axel.klinger@web.de>
2019-01-20 18:10:35 +0100

    Add some descriptions

--- app/src/main/java/com/example/myspeechtest/Eliza.java
+++ app/src/main/java/com/example/myspeechtest/Eliza.java
@@ -5,28 +5,38 @@ import android.text.TextUtils;
 public class Eliza {
 
 
-    private static final String[] CONVERSATION_KEYWORDS = {"CAN YOU", "CAN I", "YOU ARE", "YOURE", "I DONT", "I FEEL", "WHY DONT YOU", "WHY CANT I",
-            "ARE YOU", "I CANT", "I AM", " IM ", "YOU", "I WANT", "WHAT", "HOW", "WHO", "WHERE",
-            "WHEN", "WHY", "NAME", "CAUSE", "SORRY", "DREAM", "HELLO", "HI", "MAYBE", "NO", "YOUR",
-            "ALWAYS", "THINK", "ALIKE", "YES", "FRIEND", "COMPUTER", "NOKEYFOUND" };
-
-    private static final String[] CONVERSATION_KEYWORDS_DE = {"KANNST DU", "KANN ICH", "DU BIST", "DUBIST", "I DONT", "ICH FÜHLE", "WARUM NICHT DU", "WARUM KANN ICH NICHT",
+    private static final String[] CONVERSATION_KEYWORDS_DE = {
+            "KANNST DU", // 1 -> 1-4
+            "KANN ICH",  // 2 -> 4-6
+            "DU BIST",   // 3 -> 6-10
+            "DUBIST",    // 4 -> 6-10
+            "I DONT",
+            "ICH FÜHLE", "WARUM NICHT DU", "WARUM KANN ICH NICHT",
             "BIST DU", "ICH KANN NICHT", "ICH BIN", " ICHBIN ", "DU", "ICH WILL", "WAS", "WIE", "WER", "WO",
             "WANN", "WARUM", "NAME", "GRUND", "ENTSCHULDIGE", "TRAUM", "HALLO", "HI", "VIELLEICHT", "NEIN", "DEIN",
             "IMMER", "DENKE", "GLEICH", "JA", "FREUND", "COMPUTER", "NOKEYFOUND" };
 
-    private static String[] WORDS_TO_REPLACE = {"ARE","AM","WERE","WAS","YOU","I","YOUR","MY",
-            "IVE","YOUVE","IM","YOURE", "YOU", "ME"};
-
-    private static String[] WORDS_TO_REPLACE_DE = {"BIST","BIN","WAREN","WAR","DU","ICH","DEIN","MEIN",
-            "ICH HABE","DU HAST","ICH BIN","DU BIST", "DIR", "MIR"};
+    // der Vorteil dieses Ansatzes ist, dass man keine Zwischenlösung braucht
+    private static String[] WORDS_TO_REPLACE_DE = {
+            "BIST","BIN",
+            "WAREN","WAR",
+            "DU","ICH",
+            "DEIN","MEIN",
+            "ICH HABE","DU HAST",
+            "ICH BIN","DU BIST",
+            "DIR", "MIR"};
 
     private static String[] QUESTIONS = {
-            "GLAUBST DU NICHT, DASS ICH DAS KANN.", "VIELLEICHT WÄRST DU GERN IN DER LAGE DAZU.",
-            "DU MÖCHTEST, DASS ICH IN DER LAGE BIN ZU*", "VIELLEICHT WILLST DU NICHT*",
-            "MÖCHTEST DU IN DER LAGE SEIN ZU*", "WARUM DENKST DU ICH BIN*",
-            "FREUT ES DICH ZU GLAUBEN DASS ICH*BIN", "VIELLEICHT MÖCHTEST DU*SEIN",
-            "WÜNSCHT DU DIR MANCHMAL DU WÄRST*", "DON'T YOU REALLY*", "WHY DON'T YOU*",
+            "GLAUBST DU NICHT, DASS ICH*KANN.", // 1
+            "VIELLEICHT WÄRST DU GERN IN DER LAGE ZU*.", // 2
+            "DU MÖCHTEST, DASS ICH IN DER LAGE BIN ZU*", // 3
+            "VIELLEICHT WILLST DU NICHT*", // 4
+            "MÖCHTEST DU IN DER LAGE SEIN ZU*", // 5
+            "WARUM DENKST DU ICH BIN*", // 6
+            "FREUT ES DICH ZU GLAUBEN DASS ICH*BIN", // 7
+            "VIELLEICHT MÖCHTEST DU*SEIN", // 8
+            "WÜNSCHT DU DIR MANCHMAL DU WÄRST*", // 9
+            "DON'T YOU REALLY*", "WHY DON'T YOU*", // 10
             "WÄRST DU GERN IN DER LAGE ZU*", "HAST DU EIN PROBLEM DAMIT?",
             "ERZÄHL MIR MEHR ÜBER SOLCHE GEFÜHLE*", "FÜHLST DU OFT*",
             "GEFÄLLT ES DIR ZU FÜHLEN*", "GLAUBST DU WIRKLICH, DASS ICH ES NICHT*",
@@ -80,7 +90,12 @@ public class Eliza {
     };
 
     private static char[] CONVERSATION_TO_RESPONSES_MAP = {
-            1,3,4,2,6,4,6,4,10,4,14,3,17,3,20,2,22,3,25,3,
+            1,3,    // 1-4
+            4,2,    // 4-6
+            6,4,    // 6-10
+            6,4,
+            10,4,
+            14,3,17,3,20,2,22,3,25,3,
             28,4,28,4,32,3,35,5,40,9,40,9,40,9,40,9,40,9,40,9,
             49,2,51,4,55,4,59,4,63,1,63,1,64,5,69,5,74,2,76,4,
             80,3,83,7,90,3,93,6,99,7,106,6};
@@ -108,7 +123,7 @@ public class Eliza {
         input = " " + input.toUpperCase().replace("\'", "") + " ";
 
         if (previousInput != null && input.equals(previousInput)) {
-            return "DIDN'T YOU JUST SAY THAT?\n";
+            return "HAST DU DAS NICHT GERADE GESAGT?\n";
         }
         previousInput = input;
 
